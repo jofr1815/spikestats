eadd7506b6c8c00331481b6161ca893d005bbf75
jofr1815 <jofr1815@colorado.edu>
2024-03-31 20:45:39 -0600

    Updated process_plays to include opposing team. WARNING: CLI use and argument parser not yet implemented! You may pull the function into other modules, but in its current state it WILL NOT work as a standalone script

--- scripts/process_plays.py
+++ scripts/process_plays.py
@@ -1,5 +1,8 @@
 from sys import argv
+from glom import glom
 import pandas as pd
+import argparse
+import os
 
 
 df = pd.read_csv(argv[1], low_memory=False)
@@ -8,8 +11,9 @@ init_length = len(df)
 sets = dict()
 
 class Set:
-    def __init__(self, setname, season, wonSet, wonMatch, opponent):
+    def __init__(self, setname, season, wonSet, wonMatch, teamName, opponent):
         self.setname = setname
+        self.teamName = teamName
         self.opponent = opponent
         self.season = season
         self.wonSet = wonSet
@@ -25,9 +29,9 @@ class Set:
         self.earnedPts = 0
 
     def export(self):
-        return(f"\n{self.setname},{self.opponent},{self.season},{self.wonSet},{self.wonMatch},{self.unforcedErrs},{self.earnedPts},{self.serveErr},{self.setErr},{self.attackErr},{self.freeballErr},{self.ace},{self.kill},{self.stuffBlock}")
+        return(f"\n{self.setname},{self.teamName},{self.opponent},{self.season},{self.wonSet},{self.wonMatch},{self.unforcedErrs},{self.earnedPts},{self.serveErr},{self.setErr},{self.attackErr},{self.freeballErr},{self.ace},{self.kill},{self.stuffBlock}")
 
-colNames = "setname,opponent,season,wonSet,wonMatch,unforcedErrs,earnedPts,serveErr,setErr,attackErr,freeballErr,ace,kill,stuffBlock"
+colNames = "setname,teamName,opponent,season,wonSet,wonMatch,unforcedErrs,earnedPts,serveErr,setErr,attackErr,freeballErr,ace,kill,stuffBlock"
 
 def fixSeasonName(season):
     if "CU" in season:
@@ -36,34 +40,37 @@ def fixSeasonName(season):
 
 def processSets(df, cuPerspect = True):
 
-    df = df.loc[
-    (df["team"] == "CU") &
-    ((df["evaluation_code"] == "=") |
-    ((df["evaluation_code"] == "#") & (df["skill"].isin(["Serve", "Attack", "Block"]))))
-    ]
+    if cuPerspect == True:
+        df = df.loc[
+        (df["team"] == "CU")]
+    else:
+        df = df.loc[
+            (df["team"]!= "CU")
+        ]
 
     filtered_len = len(df)
     print(f"Filtered out {filtered_len} plays from a total of {init_length} ({100*filtered_len/init_length:.2f}%)")
     sets = dict()
+    df = df.loc[
+        (((df["evaluation_code"] == "=") & (df["skill"].isin(["Serve", "Attack", "Block"]))) |
+        ((df["evaluation_code"] == "#") & (df["skill"].isin(["Serve", "Set", "Attack", "Free Ball"]))))
+        ]
 
     for i in range(len(df)):
         currRow = df.iloc[i]
         if currRow["match_set"] not in sets:
-            sets[currRow["match_set"]] = Set(currRow["match_set"], fixSeasonName(currRow["season"]), currRow["won_set"], currRow["won_match"], currRow["opp_team_mw"])
+            sets[currRow["match_set"]] = Set(currRow["match_set"], fixSeasonName(currRow["season"]), currRow["won_set"], currRow["won_match"], currRow["team"], currRow["opponent"])
         if currRow["evaluation_code"] == "=":
+            sets[currRow["match_set"]].unforcedErrs += 1
             match currRow["skill"]:
                 case "Serve":
                     sets[currRow["match_set"]].serveErr += 1
-                    sets[currRow["match_set"]].unforcedErrs += 1
                 case "Set":
                     sets[currRow["match_set"]].setErr += 1
-                    sets[currRow["match_set"]].unforcedErrs += 1
                 case "Attack":
                     sets[currRow["match_set"]].attackErr += 1
-                    sets[currRow["match_set"]].unforcedErrs += 1
                 case "Freeball":
                     sets[currRow["match_set"]].freeballErr += 1
-                    sets[currRow["match_set"]].unforcedErrs += 1
 
         elif currRow["evaluation_code"] == "#":
             sets[currRow["match_set"]].earnedPts += 1
@@ -84,4 +91,5 @@ def processSets(df, cuPerspect = True):
                 f.write(sets[set].export())
             f.write("\n")
 
-processSets(df, cuPerspect=True)
+if __name__ == "Main":
+
